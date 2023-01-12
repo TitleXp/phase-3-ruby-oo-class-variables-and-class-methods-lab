b4ba332b05d3c0e5a306a777de87c59063b95d8f
Surapat Mekvanich <titleyugio@gmail.com>
2023-01-12 10:45:54 -0800

    commit tree 58ddbca065137a823a29cc8d362d53b16c8d151c

---
+++ lib/song.rb
@@ -0,0 +1,38 @@
+require 'pry'
+
+class Song
+
+    attr_reader :name, :artist, :genre
+
+    @@count = 0
+    @@artists = []
+    @@genres = []
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@artists << artist
+        @@genres << genre
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.artists
+        unique_array = @@artists.uniq
+        unique_array
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+   
+end
+
+
+new_song = Song.new("Fly me to the moon", "Artist", "Big Band")
+
